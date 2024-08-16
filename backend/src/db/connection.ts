ef6ba53634fcf18dad96f66fe78aea816e7d5ab5
Nandini Agrawal <100060301+nandiniagrawal2003@users.noreply.github.com>
2024-08-16 20:51:26 +0530

    Add files via upload

---
+++ backend/src/db/connection.ts
@@ -0,0 +1,21 @@
+import {connect, disconnect} from "mongoose";
+
+async function connectToDatabase(){
+    try {
+        await connect(process.env.MONGODB_URL);
+    } catch (error) {
+        console.log(error);
+        throw new Error("Cannot connect to the database");
+    }
+}
+
+async function disconnectFromDatabase(){
+    try {
+        await disconnect();
+    } catch (error) {
+        console.log(error);
+        throw new Error("Cannot disconnect from the database");
+    }
+}
+
+export {connectToDatabase, disconnectFromDatabase};
